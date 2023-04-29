597b194fc67cca1ec006ad786db1bb3a6c2797da
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-29 13:13:24 +0900

    자동화 되지 않은 테스트

--- src/test/java/com/example/cafekiosk/CafekioskApplicationTests.java
+++ src/test/java/com/example/cafekiosk/CafekioskApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.cafekiosk;
 
+import com.example.cafekiosk.unit.CafeKiosk;
+import com.example.cafekiosk.unit.beverage.Americano;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CafekioskApplicationTests {
 
   @Test
-  void contextLoads() {
-  }
+  void add() {
+    CafeKiosk cafeKiosk = new CafeKiosk();
+    cafeKiosk.add(new Americano());
 
+    System.out.println(">>> 담긴 음료 수: " + cafeKiosk.getBeverages().size());
+    System.out.println(">>> 담긴 음료: " + cafeKiosk.getBeverages().get(0).getName());
+  }
 }
